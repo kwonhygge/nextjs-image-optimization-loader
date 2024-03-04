543e0cc3d9fa797a9c5b558ba3f52a81c8d59a5b
kwonhygge <cleancode.kwon@gmail.com>
2024-03-04 22:40:01 +0900

    fix: add babel

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var nextCustomImageLoader = require('./next-custom-image-loader');
+var webpackImageOptimizationLoader = require('./webpack-image-optimization-loader');
+module.exports = {
+  nextCustomImageLoader: nextCustomImageLoader,
+  webpackImageOptimizationLoader: webpackImageOptimizationLoader
+};
