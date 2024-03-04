dfeb1ef4a0f52e70646b61eb2bdbb79be016d47b
kwonhygge <cleancode.kwon@gmail.com>
2024-03-04 21:50:07 +0900

    first commit

---
+++ next-custom-image-loader.js
@@ -0,0 +1,49 @@
+import RESULT from './result.json';
+
+const SCREEN_BREAK_POINT = {
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+};
+
+const getSizeName = (width) => {
+    const breakpoints = Object.keys(SCREEN_BREAK_POINT);
+
+    for (const breakpoint of breakpoints) {
+        if (width <= SCREEN_BREAK_POINT[breakpoint]) {
+            return breakpoint;
+        }
+    }
+
+    return 'optimized';
+};
+
+export default function NextCustomImageLoader({ src, width }) {
+    const regex = /\/_next\/static\/media\/([^\/.]+)\.[^\/.]+(\.\w+)?$/;
+
+    const isModuleImportMatch = src.match(regex);
+
+    if (process.env.NODE_ENV === 'development') return src;
+
+    if (isModuleImportMatch) {
+        const fileName = isModuleImportMatch[1];
+        const sizeName = getSizeName(width);
+
+        if (RESULT[fileName]) {
+            const fileSrc = RESULT[fileName][sizeName];
+
+            if (fileSrc) {
+                return fileSrc;
+            } else {
+                console.error(
+                    `Image size ${sizeName} not found for ${fileName} in result.json`,
+                );
+            }
+        }
+
+        return src;
+    }
+
+    return src;
+}
---
+++ webpack-image-optimization-loader.js
@@ -0,0 +1,147 @@
+const sizeOf = require('image-size');
+const sharp = require('sharp');
+
+const fs = require('fs');
+const path = require('path');
+
+const SCREEN_BREAK_POINT = {
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+};
+
+const resultFolderPath = path.join(process.cwd(), 'public', 'optimized');
+const resultFilePath = path.join(__dirname, 'result.json');
+
+const createResizedFolderIfNotExists = () => {
+    if (!fs.existsSync(resultFolderPath)) {
+        fs.mkdirSync(resultFolderPath);
+    }
+};
+
+const updateImageInfo = (imageInfo, fileName, breakpoint, fileSrc) => {
+    imageInfo[fileName] = {
+        ...imageInfo[fileName],
+        [breakpoint]: fileSrc,
+    };
+};
+
+const optimizeOriginalImage = async (file, imageInfo, fileName, filePath) => {
+    updateImageInfo(imageInfo, fileName, 'original', filePath);
+
+    updateImageInfo(
+        imageInfo,
+        fileName,
+        'optimized',
+        path.join('/optimized', `${fileName}.webp`),
+    );
+
+    await sharp(file)
+        .webp()
+        .toFile(path.join(resultFolderPath, `${fileName}.webp`), (err) => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+        });
+};
+
+const saveResultToFile = (imageInfo) => {
+    fs.writeFileSync(resultFilePath, JSON.stringify(imageInfo));
+};
+
+const processImage = async (file, fileName, imageInfo, filePath) => {
+    const fileSize = sizeOf(file);
+
+    createResizedFolderIfNotExists();
+
+    try {
+        await Promise.all(
+            Object.entries(SCREEN_BREAK_POINT).map(
+                async ([breakpoint, breakpointWidth]) => {
+                    const isResizingNeeded = breakpointWidth < fileSize.width;
+
+                    const fileSrc = isResizingNeeded
+                        ? `${fileName}.${breakpoint}.webp`
+                        : `${fileName}.webp`;
+
+                    updateImageInfo(
+                        imageInfo,
+                        fileName,
+                        breakpoint,
+                        path.join('/optimized', fileSrc),
+                    );
+
+                    return new Promise((resolve) => {
+                        if (isResizingNeeded) {
+                            sharp(file)
+                                .resize(breakpointWidth)
+                                .webp()
+                                .toFile(
+                                    path.join(
+                                        resultFolderPath,
+                                        `${fileName}.${breakpoint}.webp`,
+                                    ),
+                                    (err) => {
+                                        if (err) {
+                                            console.log(err);
+                                            throw err;
+                                        } else {
+                                            resolve();
+                                        }
+                                    },
+                                );
+                        }
+
+                        resolve();
+                    });
+                },
+            ),
+        );
+
+        await optimizeOriginalImage(file, imageInfo, fileName, filePath);
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+};
+
+module.exports = async function () {
+    const callback = this.async();
+
+    const filePath = path.join(
+        '/',
+        path.relative(`${process.cwd()}/public`, this.resourcePath),
+    );
+
+    const file = fs.readFileSync(this.resourcePath);
+    const fileName = path.basename(filePath, path.extname(filePath));
+
+    try {
+        let imageInfo = {};
+
+        if (fs.existsSync(resultFilePath)) {
+            const resultJsonFile = fs.readFileSync(resultFilePath);
+            imageInfo = JSON.parse(resultJsonFile);
+        } else {
+            fs.writeFileSync(resultFilePath, JSON.stringify({}));
+        }
+
+        if (imageInfo[fileName] && imageInfo[fileName].original !== filePath) {
+            console.error(
+                `Error: Image with name ${fileName} already exists in result.js  \n`,
+                `duplicated path: ${filePath} and ${imageInfo[fileName].original}`,
+            );
+            process.exit(1);
+        }
+
+        await processImage(file, fileName, imageInfo, filePath);
+
+        saveResultToFile(imageInfo);
+
+        callback(null, file);
+    } catch (e) {
+        callback(e);
+    }
+};
