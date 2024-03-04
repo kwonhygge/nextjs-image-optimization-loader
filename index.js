9e5759467ff380b6a26a791b58e743cc6695eeef
kwonhygge <cleancode.kwon@gmail.com>
2024-03-04 22:05:30 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const nextCustomImageLoader = require('./next-custom-image-loader')
+const webpackImageOptimizationLoader = require('./webpack-image-optimization-loader')
+
+module.exports = {
+    nextCustomImageLoader,
+    webpackImageOptimizationLoader
+}
